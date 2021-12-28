c7d8df7206514b98c9f654da09a72df407e05995
Ricardo Zavala <rzavala192008@yahoo.com>
2021-12-28 13:51:00 -0800

    finished product

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+import MainCard from './MainCard';
+
+/**
+Different size logos to test
+  https://www.spamweed.com/wp-content/uploads/2018/03/000707-online-digital-eye-logo-free-logo-maker-02.png
+*/
+
+function App() {
+  return (
+    <div className='container'>
+      <MainCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Modal.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const Modal = (props) => {
+  return (
+    <div className='modal' data-status={props.status}>
+      <div className='modal-card'>
+        <div className='modal-header'>
+          <h2 className='title'>{props.data.title}</h2>
+        </div>
+        <div className='modal-body'>
+          <div className='subtitle'>
+            We would like to thank you for your generous donation of{' '}
+          </div>
+          <span className='donation'>${props.value.toFixed(2)}</span>
+          <div className='subtitle'>
+            With your help we have raised{' '}
+            <span className='donation-thin'>${props.raised}</span> of our{' '}
+            <span className='donation-thin'>${props.data.goal}</span> goal.{' '}
+          </div>
+          <button className='modal-btn' onClick={props.onClick}>
+            Donate some more!
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
